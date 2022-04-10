ad413cfcc774616d83f0f7205d27f6a997bf2c9f
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-04-11 08:03:55 +0900

    Add go_router

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_kokkai_gijiroku/view/home.dart';
+import 'package:flutter_kokkai_gijiroku/view/home_screen.dart';
+import 'package:go_router/go_router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final router = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (BuildContext context, GoRouterState state) => HomeScreen(
+            key: state.pageKey,
+          ),
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
       title: '国会 議事録検索システム(非公式)',
       theme: ThemeData(
         useMaterial3: true,
@@ -18,7 +32,6 @@ class MyApp extends StatelessWidget {
         colorSchemeSeed: Colors.teal,
         brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
