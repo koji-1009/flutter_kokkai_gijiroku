cef0629dc4d42acbdd14bc8bca635bd151fffd3b
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-09-03 00:54:59 +0900

    Router + Provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,91 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_kokkai_gijiroku/view/about_screen.dart';
-import 'package:flutter_kokkai_gijiroku/view/home_screen.dart';
-import 'package:flutter_kokkai_gijiroku/view/search/search_meeting_detail_screen.dart';
-import 'package:flutter_kokkai_gijiroku/view/search/search_meeting_summary_screen.dart';
-import 'package:flutter_kokkai_gijiroku/view/search/search_speech_screen.dart';
-import 'package:flutter_kokkai_gijiroku/view/status/issue_detail_screen.dart';
-import 'package:flutter_kokkai_gijiroku/view/status/speech_detail_screen.dart';
+import 'package:flutter_kokkai_gijiroku/view/router.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final _router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-      routes: [
-        GoRoute(
-          path: 'about',
-          name: AboutScreen.screenName,
-          builder: (context, state) => const AboutScreen(),
-        ),
-        GoRoute(
-          path: 'search/detail',
-          name: SearchMeetingDetailScreen.screenName,
-          pageBuilder: (context, state) => MaterialPage(
-            key: state.pageKey,
-            child: SearchMeetingDetailScreen(
-              params: state.queryParams,
-            ),
-          ),
-        ),
-        GoRoute(
-          path: 'search/summary',
-          name: SearchMeetingSummaryScreen.screenName,
-          pageBuilder: (context, state) => MaterialPage(
-            key: state.pageKey,
-            child: SearchMeetingSummaryScreen(
-              params: state.queryParams,
-            ),
-          ),
-        ),
-        GoRoute(
-          path: 'search/speech',
-          name: SearchSpeechScreen.screenName,
-          pageBuilder: (context, state) => MaterialPage(
-            key: state.pageKey,
-            child: SearchSpeechScreen(
-              params: state.queryParams,
-            ),
-          ),
-        ),
-        GoRoute(
-          path: 'speech/:speechID',
-          name: SpeechDetailScreen.screenName,
-          pageBuilder: (context, state) => MaterialPage(
-            key: state.pageKey,
-            child: SpeechDetailScreen(
-              speechID: state.params['speechID']!,
-            ),
-          ),
-        ),
-        GoRoute(
-          path: 'issue/:issueID',
-          name: IssueDetailScreen.screenName,
-          pageBuilder: (context, state) => MaterialPage(
-            key: state.pageKey,
-            child: IssueDetailScreen(
-              issueID: state.params['issueID']!,
-            ),
-          ),
-        ),
-      ],
-    ),
-  ],
-  urlPathStrategy: UrlPathStrategy.path,
-);
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class App extends ConsumerWidget {
+  const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
     return MaterialApp.router(
-      routeInformationParser: _router.routeInformationParser,
-      routerDelegate: _router.routerDelegate,
-      routeInformationProvider: _router.routeInformationProvider,
       title: '国会 議事録検索システム(非公式)',
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
@@ -117,6 +43,9 @@ class App extends StatelessWidget {
           textTheme: GoogleFonts.mPlus1pTextTheme(base.textTheme),
         );
       }(),
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
